eadd3283a82e5ea9b961b8a25eb692bf6ce717c0
migsamu <mufrikiler@gmail.com>
2022-02-18 12:24:34 +0100

    diseño y clase dbhelper

---
+++ app/src/main/java/org/iesfm/sqliteexercise1/DBHelper.java
@@ -0,0 +1,25 @@
+package org.iesfm.sqliteexercise1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "layers.sqlite";
+    private static final int VERSION = 1;
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/org/iesfm/sqliteexercise1/MainActivity.java
@@ -0,0 +1,21 @@
+package org.iesfm.sqliteexercise1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void crearBaseDatos(View v) {
+
+    }
+}
